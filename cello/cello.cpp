456fd23c7eb72e51a2764e7091341e19c91933f7
Eric <ewetzel@ucsb.edu>
2024-05-28 17:38:58 -0700

    progress on stuff

--- cello/cello.cpp
+++ cello/cello.cpp
@@ -1,6 +1,7 @@
 #include <cstdio>  // for printing to stdout
 #include <unordered_map>
 #include <unordered_set>
+#include <set>
 #include <algorithm>
 
 #include "Gamma/Analysis.h"
@@ -22,6 +23,19 @@ static float bowLength = 300.0f;
 static float stringLength = 250.0f;
 
 class CelloString {
+    // todo: currently unused
+    float baseFreq;
+    float length;
+
+    std::set<int> fingers;
+
+    gam::Saw<> saw;
+    gam::Sine<> vibrato;
+
+    al::Mesh mesh;
+
+public:
+    CelloString(float baseFreq, float length) : baseFreq(baseFreq), length(length) {}
 
 };
 
@@ -50,10 +64,12 @@ public:
         bow.primitive(al::Mesh::LINE_STRIP);
         bow.vertex(0, 0);
         bow.vertex(bowLength, 0);
+        bow.colorFill(1);
 
         aString.primitive(al::Mesh::LINE_STRIP);
         aString.vertex(0, 0);
         aString.vertex(0, stringLength);
+        aString.colorFill(1);
 
         vibrato.freq(5.f);
 
@@ -91,43 +107,42 @@ public:
         g.lineWidth(2);
         g.depthTesting(true);
         g.pointSize(8);
-        // g.nicest();
-        // g.stroke(8);
-        
+
+        g.meshColor();
+
         g.pushMatrix();
         g.translate(bowPos);
-        g.color(1);
         g.draw(bow);
         g.popMatrix();
 
         g.pushMatrix();
         g.translate(width() / 2, height() / 4);
-        g.color(1);
         g.draw(aString);
         g.popMatrix();
+    }
+
+    void updateString() {
+        for (int i = 6; i >= 0; --i) {
+            if (fingers[i]) {
+                saw.freq(440.0f * powf(2.0f, i / 12.0f));
+                aString.colors().clear();
+                aString.colorFill(al::HSV(i / 12.0f, 1.0f, 1.0f));
+                break;
+            }
+        }
     }
 
     bool onKeyDown(const al::Keyboard &k) override {
         if (k.key() < '1' || k.key() > '6') return true;
         fingers[k.key() - '0'] = true;
-        for (int i = 6; i >= 0; --i) {
-            if (fingers[i]) {
-                saw.freq(440.0f * powf(2.0f, i / 12.0f));
-                break;
-            }
-        }
+        updateString();
         return true;
     }
 
     bool onKeyUp(const al::Keyboard &k) override {
         if (k.key() < '1' || k.key() > '6') return true;
         fingers[k.key() - '0'] = false;
-        for (int i = 6; i >= 0; --i) {
-            if (fingers[i]) {
-                saw.freq(440.0f * powf(2.0f, i / 12.0f));
-                break;
-            }
-        }
+        updateString();
         return true;
     }
 
